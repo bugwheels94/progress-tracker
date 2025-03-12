6edce78f909c69b4d117e3e81ae40fa289de7576
bugwheels94 <bugwheels94@gmail.com>
2025-03-12 21:09:58 +0000

    enhance site

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Project from "./Project";
-import Home from "./scene/Home/Home";
-import Nudge from "./scene/Nudge/Nudge";
-import Remind from "./scene/Remind/Remind";
-
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -19,24 +14,7 @@ const queryClient = new QueryClient({
 });
 const router = createBrowserRouter([
   {
-    path: "/remind",
-    element: <Nudge />,
-  },
-  {
-    path: "/remind/:activityId",
-    element: <Remind />,
-  },
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/app",
-    element: <App />,
-    loader: () => "Loading",
-  },
-  {
-    path: "/projects/:projectId",
+    path: "/:tag?",
     element: <Project />,
     loader: () => "Loading",
   },
--- src/services/tasks.ts
+++ src/services/tasks.ts
@@ -1,4 +1,3 @@
-import { useQuery } from "@tanstack/react-query";
 import { nanoid } from "nanoid";
 import PouchDB from "pouchdb";
 import find from "pouchdb-find";
@@ -7,150 +6,70 @@ PouchDB.plugin(find);
 const db = new PouchDB<ReturnType<typeof createActivity>>("activities");
 db.createIndex({
   index: {
-    fields: ["projectId"],
-  },
-});
-db.createIndex({
-  index: {
     fields: ["status", "createdAt"],
   },
 });
 export enum Status {
-  Paused,
-  Idle,
-  Active,
-  Done,
+  Idle = 1,
+  Active = 0,
+  Done = 2,
 }
 function createActivity() {
   return {
-    lastActive: Date.now(),
-    timeSpent: 0,
-    status: Status.Idle,
+    inProgress: false,
     createdAt: Date.now(),
-    projectId: "",
+    estimation: 0,
+    tag: "",
     title: "",
+    finishedOn: [] as string[], // days in which the task was finished
+    repeatsDaily: false, // days
+    _id: nanoid(),
   };
 }
-export async function getActiveActivity() {
-  try {
-    const docs = await db.find({
-      selector: {
-        status: Status.Active,
-      },
-    });
-    return docs.docs[0];
-  } catch (e) {
-    console.log("hii", e);
-  }
-}
-export async function getActivity(id: string) {
-  try {
-    const docs = await db.get(id);
-    return docs;
-  } catch (e) {
-    console.log("hii", e);
-  }
-}
-export function useGetActivity(activityId: string) {
-  return useQuery({
-    queryKey: ["activity", activityId],
-    queryFn: async function getActivity() {
-      try {
-        const docs = await db.get(activityId);
-        return docs;
-      } catch (e) {
-        console.log("hii", e);
-      }
-    },
-  });
+export function getActivityStatus(a: Activity) {
+  const today = new Date().toLocaleDateString("en-CA");
+
+  return a.inProgress
+    ? Status.Active
+    : (a.finishedOn?.length && !a.repeatsDaily) ||
+      ((a.finishedOn || []).includes(today) && a.repeatsDaily)
+    ? Status.Done
+    : Status.Idle;
 }
 export type Activity = ReturnType<typeof createActivity>;
-export async function getActivities(projectId: string) {
+export async function getActivities(tag: string): Promise<Activity[]> {
   try {
-    const docs = await db.find({
-      selector: { projectId },
+    const docs = tag
+      ? await db.find({ selector: { tag } })
+      : await db.find({ selector: {} });
+
+    let docs2: Activity[] = docs.docs as Activity[];
+
+    docs2.sort((a, b) => {
+      if (!a || !b) return 0;
+      const statusComparison = getActivityStatus(a) - getActivityStatus(b);
+      if (statusComparison !== 0) {
+        return statusComparison;
+      }
+
+      // If statuses are the same, sort by createdAt (earliest first)
+      return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
     });
-    console.log("k2", docs);
-    let docs2 = docs.docs;
-    docs2
-      .map((doc) => {
-        return {
-          ...doc,
-          status:
-            Date.now() - (doc.lastActive || 0) > 25 * 60 * 1000 &&
-            doc.status === Status.Active
-              ? Status.Idle
-              : doc.status,
-        };
-      })
-      .sort((a, b) => {
-        const statusComparison = a.status - b.status;
-        if (statusComparison !== 0) {
-          return statusComparison; // Sort by status first
-        }
-        return (
-          new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
-        );
-      });
+
     return docs2;
   } catch (e) {
-    console.log("hii", e);
+    console.error("Error fetching activities:", e);
+    return []; // Ensure it always returns Activity[]
   }
 }
-export async function postActivity(doc: { projectId: string; title: string }) {
+export async function putActivity(
+  doc: Partial<Activity>,
+  original: Activity = createActivity()
+) {
   try {
     await db.put({
-      ...createActivity(),
+      ...original,
       ...doc,
-      _id: nanoid(),
-    });
-  } catch (e) {
-    console.log(e);
-  }
-}
-export async function patchActivity({
-  old,
-  young,
-}: {
-  old: Activity;
-  young: Partial<Activity>;
-}) {
-  try {
-    console.log(young);
-    if (young.status === Status.Active) {
-      console.log("zxc updating to active");
-      await db
-        .find({
-          selector: {
-            status: Status.Active,
-          },
-        })
-        .then((docs) => {
-          console.log("zxc found active", docs);
-          return db.bulkDocs(
-            docs.docs.map((doc) => {
-              return {
-                ...doc,
-                status: Status.Paused,
-                timeSpent: doc.timeSpent + (Date.now() - doc.lastActive),
-              };
-            })
-          );
-        });
-    }
-
-    await db.put({
-      ...old,
-      ...young,
-      ...(young.status === Status.Done || young.status === Status.Paused
-        ? {
-            timeSpent: old.timeSpent + (Date.now() - old.lastActive),
-          }
-        : young.status === Status.Active
-        ? {
-            lastActive: Date.now(),
-          }
-        : {}),
     });
   } catch (e) {
     console.log(e);
